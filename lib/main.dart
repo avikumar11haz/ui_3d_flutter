e77e009f39e630760a87d1c6bd82494817257a23
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-12-03 23:32:52 +0530

    add assets files and add script

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:o3d/o3d.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,26 +16,57 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  O3DController o3dController = O3DController();
+  PageController mainPageController = PageController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
+        children: [
+          O3D(
+            src: 'assets/disney_style_character.glb',
+            controller: o3dController,
+            ar: false,
+            autoPlay: true,
+            autoRotate: false,
+          ),
+          PageView(
+            controller: mainPageController,
+            children: [],
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.analytics_outlined),
+            label: 'home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timer_outlined),
+            label: 'home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outlined),
+            label: 'home',
+          )
+        ],
       ),
     );
   }
